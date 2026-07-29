86bb9929c17a720449ee1a894846fc67231c7e50
wisdomntui <wisdom.ntui@gmail.com>
2026-07-29 17:28:42 +0100

    feat: Task manager

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -22,7 +22,7 @@ return [
     'allowed_origins' =>  [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'frontend-production-aa6e4.up.railway.app'
+        'https://frontend-production-aa6e4.up.railway.app'
     ],
 
     'allowed_origins_patterns' => [],
